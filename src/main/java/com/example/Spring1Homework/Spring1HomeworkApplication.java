1d54d640f40ee44b3f65f7272ca5101169eac1e7
Redyx <dr.redyx@gmail.com>
2022-01-25 23:06:06 +0300

    Initial commit

---
+++ src/main/java/com/example/Spring1Homework/Spring1HomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.example.Spring1Homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring1HomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring1HomeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Spring1Homework/Spring1HomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Spring1Homework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring1HomeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
